71a0710125fb934cfaf6d29889461487342d5939
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-11-08 13:29:34 -0500

    Added Playground

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./index.css";
+import App from "./App.jsx";
+import Playground from "./Pages/Playground.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Playground" element={<Playground />} />
+      </Routes>
+    </Router>
+  </StrictMode>
+);
